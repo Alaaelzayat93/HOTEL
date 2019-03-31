e54c2ec70a100ce5ef50146f60c54039c3e0d782
eng alaa_elzayat <alaaelzayat2018@gmail.com>
2019-03-31 20:19:13 +0200

    new

--- js/main.js
+++ js/main.js
@@ -71,6 +71,26 @@ function openPage(pageName, elmnt, color) {
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
+/*slider owl*/
+$(".owl-carousel").owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    option:true,
+    dots:true,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
 
 
 
